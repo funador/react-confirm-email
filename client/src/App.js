8914fb90a1edb39ca2e3ee77761c23b2eae09b50
funador <jesse.heaslip@gmail.com>
2019-02-08 09:55:32 -0600

    removing css import from notify package

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 import Notifications from 'react-notify-toast'
-import 'react-toastify/dist/ReactToastify.css'
+// import 'react-toastify/dist/ReactToastify.css'
 
 import Landing from './components/Landing'
 import Confirm from './components/Confirm'
